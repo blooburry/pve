b5719bf28ae2544966ea4af03784f47da87003ff
blooburry <altaccstotzka031@gmail.com>
2024-04-30 18:11:43 +0200

    initial commit

---
+++ embedded/MqttService.h
@@ -0,0 +1,27 @@
+#ifndef MQTT_SERVICE_H
+#define MQTT_SERVICE_H
+
+#include <iostream>
+#include <string>
+#include <mosquitto.h>
+
+class MqttService {
+public:
+    MqttService();
+    ~MqttService();
+
+    void sendMagData(float magData);
+    void sendAccData(float accData);
+    void sendGyroData(float gyroData);
+
+private:
+    std::string brokerAddress = "localhost";
+    int brokerPort = 1883;
+    struct mosquitto *mosq;
+
+    void connectToBroker();
+    void disconnectFromBroker();
+    void publishData(const std::string& topic, float data);
+};
+
+#endif
