a3cef614a80353a4fbb749b0131eae10adc5cc63
KangarooAteMe <bastiaan.wolters@proton.me>
2024-05-03 13:40:11 +0200

    main file voor de lijnsensoren
    originele lijnsensor.ino aangepast om meer OOP te gebruiken.

--- embedded/lijnsensor.ino
+++ embedded/lijnsensor.ino
@@ -1,39 +1,21 @@
 #include <Zumo32U4.h>
-#include "MqttService.h"
-#include <Arduino_Json.h>
+//#include "MqttServicelijn.h"
+//#include <Arduino_Json.h>
+#include "Calibraties.h"
+#include "SerialPrinter.h"
 
 
 Zumo32U4LineSensors lijnsensor;
+Calibraties calibratiemachine(lijnsensor);
+SerialPrinter printer;
 
 uint16_t lijnsensorWaardes[3] = { 0, 0, 0 };
 
 void setup() {
   // put your setup code here, to run once:
   lijnsensor.initThreeSensors();
-  calibreer();
+  calibratiemachine.calibratiereken();
 
-
-}
-
-void calibreer() {
-
-  for (uint32_t i = 0; i < 500; i++) {
-    lijnsensor.calibrate();
-    Serial.print(F("bezig met calibreren"));
-    ledGreen(1);
-  }
-  ledGreen(0);
-}
-
-void printNaarSerial() {
-  static char ruimte[50];
-  sprintf(ruimte, "%4d %4d %4d\n", 
-  
-  lijnsensorWaardes[0], 
-  lijnsensorWaardes[1], 
-  lijnsensorWaardes[2]);
-
-  Serial.print(ruimte);
 
 }
 
@@ -43,9 +25,10 @@ void loop() {
   if ((uint32_t) (millis() - tijd) >= 100) {
     tijd = millis();
     lijnsensor.readCalibrated(lijnsensorWaardes);
-    printNaarSerial();
+    printer.printje(lijnsensorWaardes);
+  }
 
-    StaticJsonDocument<100> lijnJson; 
+   /* StaticJsonDocument<100> lijnJson; 
 
     // Fill JSON objects with sensor data
     lijnJson["sensor 1"] = lijnsensorWaardes[0];
@@ -62,7 +45,7 @@ void loop() {
     mqttService.sendLijnData(lijnData.c_str());
 
 
-    delay(1000);
-  }
+    delay(1000); */
+
   
 }
