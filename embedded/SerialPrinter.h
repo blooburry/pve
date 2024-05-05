a128beddfb2f2b491a2980c49d50bd1e2e7ce1bd
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2024-05-05 14:04:52 +0200

    comments added

--- embedded/SerialPrinter.h
+++ embedded/SerialPrinter.h
@@ -3,12 +3,16 @@
 
 #include <stdint.h>
 
+
+/* 
+this class is used to print the calibrated values into an array
+*/
 class SerialPrinter
 {   
 public:
-    SerialPrinter();
-    ~SerialPrinter();
-    void printje(uint16_t* lijnSensorWaardes);
+    SerialPrinter(); // constructor
+    ~SerialPrinter(); // destructor
+    void printje(uint16_t* lijnSensorWaardes); // the print function takes a pointer to the array that was initialised in 'lijnsensor.ino' as an argument.
 };
 
 #endif
