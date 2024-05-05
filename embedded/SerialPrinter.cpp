a2219121d7206fc2b6cee7d18e318f62d1a89ca9
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2024-05-05 14:25:59 +0200

    comments added

--- embedded/SerialPrinter.cpp
+++ embedded/SerialPrinter.cpp
@@ -5,6 +5,11 @@ SerialPrinter::SerialPrinter(){}
 
 SerialPrinter::~SerialPrinter(){}
 
+/* 
+this method prints the values found within the lijnSensorWaardes array
+into a formatted string which is then shown on the serial monitor 
+*/
+
 void SerialPrinter::printje(uint16_t* lijnSensorWaardes){
     static char ruimte[50];
     sprintf(ruimte, "%4d %4d %4d\n",
