a1944c878973b2a6e6208d3c6406928b8a5b0d69
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2024-05-05 14:19:26 +0200

    comments added

--- embedded/Calibraties.cpp
+++ embedded/Calibraties.cpp
@@ -1,8 +1,15 @@
 #include "Calibraties.h"
-
-Calibraties::Calibraties(Zumo32U4LineSensors& lijnsensor) : lijnsensor(lijnsensor) {}
+/*
+this constuctor takes a reference to the lijnsensor object as an argument so that it can be used in the calibratiereken method 
+*/
+Calibraties::Calibraties(Zumo32U4LineSensors& lijnsensor) : lijnsensor(lijnsensor) {} 
 
 Calibraties::~Calibraties() {}
+
+/* 
+this method calibrates the line sensors for about 12.5 seconds and stores the calibrated values. 
+every 2.5 seconds the message "bezig met calibreren" is shown on the serial monitor.
+*/
 
 void Calibraties::calibratiereken() {
     for (uint32_t i = 0; i < 500; i++) {
