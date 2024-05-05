c1de760a1ae46924625fc5e5a3d980d50abb5a21
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2024-05-05 14:11:18 +0200

    comments added

--- embedded/Calibraties.h
+++ embedded/Calibraties.h
@@ -4,15 +4,17 @@
 #include <Zumo32U4.h>
 
 
-
+/*
+this class handles the calibration of the line sensors 
+*/
 class Calibraties
 {
 private:
-    Zumo32U4LineSensors& lijnsensor;
+    Zumo32U4LineSensors& lijnsensor; // reference to the lijnsensor object
 public:
-    Calibraties(Zumo32U4LineSensors& lijnsensor);
-    ~Calibraties();
-    void calibratiereken();
+    Calibraties(Zumo32U4LineSensors& lijnsensor); // constructor which takes a reference to the lijnsensor object as an argument
+    ~Calibraties(); // destructor
+    void calibratiereken(); // this method handles the calibration of the values found bu the line sensor
 };
 
 #endif
