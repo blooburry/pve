37d528cfd9773b784c594937db8ce1861f0d26c9
blooburry <altaccstotzka031@gmail.com>
2024-05-31 16:44:40 +0200

    fixed a bug with the buffer printing empty spaces

--- embedded/main/SensorDataBuffer.cpp
+++ embedded/main/SensorDataBuffer.cpp
@@ -14,7 +14,7 @@ SensorDataBuffer::SensorDataBuffer():
 { };
 
 const void SensorDataBuffer::stuurData() {
-  for(size_t i = imuNieuweDataIndex; i < imuSensorData.size(); ++i) { // iterereer door de vector, beginnend bij de eerste index nog niet gestuurd is
+  for(size_t i = imuNieuweDataIndex; i < imuSensorData.elements(); ++i) { // iterereer door de vector, beginnend bij de eerste index nog niet gestuurd is
     char report[120];
     snprintf_P(report, sizeof(report),
                PSTR("Accel: %6d %6d %6d    Mag: %6d %6d %6d    Gyro: %6d %6d %6d"),
@@ -23,25 +23,25 @@ const void SensorDataBuffer::stuurData() {
                imuSensorData[i].gyro.x, imuSensorData[i].gyro.y, imuSensorData[i].gyro.z);
     Serial.println(report);
   }
-  imuNieuweDataIndex = imuSensorData.size(); // de cursor wordt verplaatst naar de het einde van de vector, aangezien alles nu gestuurd is
+  imuNieuweDataIndex = imuSensorData.elements(); // de cursor wordt verplaatst naar de het einde van de vector, aangezien alles nu gestuurd is
 
-  for(size_t i = lijnNieuweDataIndex; i < lijnSensorData.size(); ++i) {
+  for(size_t i = lijnNieuweDataIndex; i < lijnSensorData.elements(); ++i) {
     char report[120];
     snprintf_P(report, sizeof(report),
                PSTR("Sensor1: %6d    Sensor2: %6d    Sensor3: %6d"),
                lijnSensorData[i].sensor1, lijnSensorData[i].sensor2, lijnSensorData[i].sensor3);
     Serial.println(report);
   }
-  lijnNieuweDataIndex = lijnSensorData.size();
+  lijnNieuweDataIndex = lijnSensorData.elements();
 
-  for(size_t i = proxNieuweDataIndex; i < proxSensorData.size(); ++i) {
+  for(size_t i = proxNieuweDataIndex; i < proxSensorData.elements(); ++i) {
     char report[120];
     snprintf_P(report, sizeof(report),
                PSTR("ProxSensor: %6d"),
                proxSensorData[i]);
     Serial.println(report);
   }
-  proxNieuweDataIndex = proxSensorData.size();
+  proxNieuweDataIndex = proxSensorData.elements();
 };
 
 void SensorDataBuffer::bufferDataIMU(const IMUSensorData &data) {
