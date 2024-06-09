a767a7ef71a11012ef2ca9006df84c975c96cd1f
aasepaas <146937344+aasepaas@users.noreply.github.com>
2024-06-09 19:24:57 +0200

    lijnsensor.h

--- embedded/testen_niet_volledig_werkend_lijnsensor/lijnsensor.h
+++ embedded/testen_niet_volledig_werkend_lijnsensor/lijnsensor.h
@@ -3,6 +3,7 @@
 
 #include <SimpleVector.h>
 #include <Zumo32U4.h>
+
 #include "kleurLijnBerekening.h"
 
 using namespace std;
@@ -15,14 +16,20 @@ class lijnsensor : public Zumo32U4LineSensors {
 private:
   /*! reference to the lijnsensor object */
   Zumo32U4Motors motors;
-  unsigned int LSdata[3];
+  const float Cpro = 0.7;
+  const float Cint = 0.0;
+  const float Cafg = 6.0;
+  const int dodeHoek = 100;
   kleurlijnberekening ber;
-  
+ // const int bufferSize = 5;  // Size of the buffer to store sensor readings
+  //SimpleVector<unsigned int> lijnsensordataBuffer[5];  // Buffer to store sensor readings
 
 
 public:
   /*! constructor which takes a reference to the lijnsensor object as an argument */
   SimpleVector<unsigned int> lijnsensordata;
+  unsigned int LSdata[5];
+
 
   lijnsensor();
 
