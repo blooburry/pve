17968dbeed49ed51d69378a85f482b56546e565a
aasepaas <146937344+aasepaas@users.noreply.github.com>
2024-06-10 16:01:18 +0200

    Create xbee.cpp

---
+++ embedded/KeyInterpreter_en_Xbee/xbee.cpp
@@ -0,0 +1,20 @@
+#include "xbee.h"
+
+Xbee::Xbee() {}
+
+Xbee::~Xbee() {}
+
+
+
+void Xbee::stuurDataNaarXbee(String x) {  //print dingen naar de xbee toe. Je kan de string aanpassen met andere waardes
+  Serial1.println(x);
+}
+
+String Xbee::leesDataIn() {  //deze functie leest inkomende data binnen van de xbee naar de zumo
+  if (Serial1.available() > 0) {
+    input = Serial1.readString();
+    input.trim();
+    return input;
+  }
+  return input;
+}
