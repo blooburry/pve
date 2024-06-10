450913047a2db683eb271ddd73a1d7dbae4a0da1
KangarooAteMe <bastiaan.wolters@proton.me>
2024-06-10 21:33:19 +0200

    includes en Status aangepast om handmodus te faciliteren
    Keyinterpreter en Proxysensor zijn included zodat de handmodus werkt. de proxysensor is een pointer. Handmodus is toegevoegd aan de mogelijke statussen.

--- embedded/main/StatusControl.h
+++ embedded/main/StatusControl.h
@@ -1,4 +1,7 @@
 #include "Motors.h"
+#include "KeyInterpreter.h"
+#include "ProxySensor.h"
+
 
 #ifndef STATUSCONTROL_H
 #define STATUSCONTROL_H
@@ -11,7 +14,8 @@ class StatusControl {
       VOLG_LIJN_ZWART,
       VOLG_LIJN_GROEN, // volg de lijn met halve snelheid
       ZOEK_LIJN, // hij kan geen lijn vinden dus hij rijdt langzaam vooruit
-      DUW_BLOK
+      DUW_BLOK,
+      HANDMODUS
     };
     enum VolgendeBocht { // als hij moet onthouden dat hij bij het volgende kruispunt naar links of rechts moet
       NONE,
@@ -22,11 +26,13 @@ class StatusControl {
     void proxSetSpeeds(int, int);
     void lijnSetSpeeds(int, int);
     void setStatus(Status);
+    void besturing(Commando);
 
   private:
     Status status;
     VolgendeBocht volgendeBocht;
     Motors* motors;
+    ProxySensor* prox;
 };
 
 #endif
