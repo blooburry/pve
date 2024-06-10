4a024a3d6d92fc96923496b8bd48684879ccb315
KangarooAteMe <bastiaan.wolters@proton.me>
2024-06-10 21:31:44 +0200

    Methode voor handbesturing toegevoegd
    deze methode kijkt naar toetsen die worden ingedrukt en bestuurd daarmee de zumo.

--- embedded/main/StatusControl.cpp
+++ embedded/main/StatusControl.cpp
@@ -3,6 +3,7 @@
 
 using Status = StatusControl::Status;
 using VolgendeBocht = StatusControl::VolgendeBocht;
+using Commando = KeyInterpreter::Commando;
 
 StatusControl::StatusControl(Motors* motors): 
     motors(motors), 
@@ -24,4 +25,68 @@ void StatusControl::lijnSetSpeeds(int left, int right) {
 
 void StatusControl::setStatus(Status s) {
   status = s;
+}
+
+void StatusControl::besturing(Commando c, Status s)
+{
+
+  if (c == BEGIN_MET_XBEE_GEBRUIK){
+    s = HANDMODUS;
+    }
+    if (s == HANDMODUS)
+    {
+      if (c == RECHTDOOR)
+      {
+        motors->setSpeeds(MEDIUM_SPEED, MEDIUM_SPEED);
+      }
+      if (c == ACHTERUIT)
+      {
+        motors->setSpeeds(-(MEDIUM_SPEED), -(MEDIUM_SPEED));
+      }
+
+      if (c == BOCHT_LINKS)
+      {
+        motors->setSpeeds(0, MEDIUM_SPEED);
+      }
+
+      if (c == BOCHT_RECHTS)
+      {
+        motors->setSpeeds(MEDIUM_SPEED, 0);
+      }
+
+      if (c == SNEL_RECHTDOOR)
+      {
+        motors->setSpeeds(MAX_SPEED, MAX_SPEED);
+      }
+
+      if (c == SNEL_ACHTERUIT)
+      {
+        motors->setSpeeds(-(MAX_SPEED), -(MAX_SPEED));
+      }
+
+      if (c == SCHERPE_BOCHT_LINKS)
+      {
+        motors->setSpeeds(0, MAX_SPEED);
+      }
+
+      if (c == SCHERPE_BOCHT_RECHTS)
+      {
+        motors->setSpeeds(MAX_SPEED, 0);
+      }
+
+      if (c == ZET_PROXYSENSOR_AAN)
+      {
+        prox->setup();
+      }
+
+      if (c == STOP_MOTORS)
+      {
+        motors->setSpeeds(0, 0);
+      }
+
+      if (c == STOP_MET_XBEE_GEBRUIK)
+      {
+        s = IDLE;
+      }
+    }
 }
