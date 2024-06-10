149858db77b4b6737bacccd29603fe7f61b94b1c
aasepaas <146937344+aasepaas@users.noreply.github.com>
2024-06-10 16:01:05 +0200

    Create xbee.h

---
+++ embedded/KeyInterpreter_en_Xbee/xbee.h
@@ -0,0 +1,26 @@
+//#include <Zumo32U4.h>
+
+#include <SimpleVector.h>
+//#include <iostream>
+
+
+
+using namespace std;
+
+#ifndef XBEE_H
+#define XBEE_H
+
+
+class Xbee {
+private:
+  SimpleVector<int> sensorData;
+  String input;
+
+public:
+  Xbee();
+  ~Xbee();
+  void stuurDataNaarXbee(String);
+  String leesDataIn();
+};
+
+#endif
